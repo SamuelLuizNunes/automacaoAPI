dae9090c71d4125f0d1418f3f79a06755d4c8884
Samuel Nunes <samuelluiznunes10@gmail.com>
2022-12-05 11:29:56 -0300

    cucumber, tags e datatable

--- src/test/java/steps/LoginSteps.java
+++ src/test/java/steps/LoginSteps.java
@@ -1,18 +1,37 @@
 package steps;
 
+import java.util.Map;
+
 import io.cucumber.java.pt.Dado;
+import io.cucumber.java.pt.Entao;
 import io.cucumber.java.pt.Quando;
+import io.restassured.http.ContentType;
+import maps.LoginMap;
+import utils.RestUtils;
 
 public class LoginSteps {
-
+	String url = "http://192.168.0.6:8080/";
+	
 	@Dado("que tenha um payload valido da API de Login")
 	public void queTenhaUmPayloadValidoDaAPIDeLogin() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+		LoginMap.initLogin();
+		RestUtils.setBaseURI(url);
 	}
+	
+	@Dado("que tenha um payload da API de Login com as seguintes informacoes")
+	public void queTenhaUmPayloadDaAPIDeLoginComAsSeguintesInformacoes(Map<String, Object> map) {
+		LoginMap.initLogin();
+		RestUtils.setBaseURI(url);
+		LoginMap.getLogin().putAll(map);
+	}
+	
 	@Quando("envio uma requisicao do tipo POST de Login")
 	public void envioUmaRequisicaoDoTipoPOSTDeLogin() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+		RestUtils.post(LoginMap.getLogin(), ContentType.JSON, "auth");
+	}
+	
+	@Entao("armazeno o token que recebo do response de Login")
+	public void armazenoOTokenQueReceboDoResponseDeLogin() {
+	    LoginMap.token = RestUtils.getResponse().jsonPath().get("token");
 	}
 }
